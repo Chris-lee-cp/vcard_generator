40ba168f35f4c5290bd65cef6d4ab8a03f23c1bb
Chris Lee <jhoon.chris@gmail.com>
2018-01-13 21:11:25 -0800

    Update vcard_gen.cpp

--- vcard_gen.cpp
+++ vcard_gen.cpp
@@ -5,7 +5,6 @@
 **  Description:    Simple V-Card generator.
 **
 **  Copyright  2018, junghoon lee(jhoon.chris@gmail.com)
-**
 *****************************************************************************/
 
 #include <iostream>
